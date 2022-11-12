56d031e0f2f436e5ce695a2921cc77b96f12df8f
Walter Diaz <46164488+imwaltdiaz@users.noreply.github.com>
2022-11-11 23:48:24 -0500

    Update

---
+++ src/paquete/clases/Cuenta.java
@@ -0,0 +1,15 @@
+package paquete.clases;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author wadia
+ */
+public class Cuenta {
+    
+}
